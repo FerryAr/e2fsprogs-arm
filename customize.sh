7776be7f29c8fbac337c0f2bce1dab1081393212
FerryAr <ferryakbarardiansyah@gmail.com>
2020-08-25 18:29:56 +0700

    Minor update

--- customize.sh
+++ customize.sh
@@ -1,3 +1,21 @@
 # Set permissions
 ui_print "- Setting permissions"
-set_perm_recursive $MODPATH/system 0 0 0755
+set_perm $MODPATH/system/bin/badblocks 0 0 0755
+set_perm $MODPATH/system/bin/base_device 0 0 0755
+set_perm $MODPATH/system/bin/chattr 0 0 0755
+set_perm $MODPATH/system/bin/debugfs 0 0 0755
+set_perm $MODPATH/system/bin/dumpe2fs 0 0 0755
+set_perm $MODPATH/system/bin/e2freefrag 0 0 0755
+set_perm $MODPATH/system/bin/e2fsck 0 0 0755
+set_perm $MODPATH/system/bin/e2fuzz 0 0 0755
+set_perm $MODPATH/system/bin/e2image 0 0 0755
+set_perm $MODPATH/system/bin/e2initrd_helper 0 0 0755
+set_perm $MODPATH/system/bin/e2undo 0 0 0755
+set_perm $MODPATH/system/bin/filefrag 0 0 0755
+set_perm $MODPATH/system/bin/fsck 0 0 0755
+set_perm $MODPATH/system/bin/logsave 0 0 0755
+set_perm $MODPATH/system/bin/lsattr 0 0 0755
+set_perm $MODPATH/system/bin/mke2fs 0 0 0755
+set_perm $MODPATH/system/bin/mklost+found 0 0 0755
+set_perm $MODPATH/system/bin/resize2fs 0 0 0755
+set_perm $MODPATH/system/bin/tune2fs 0 0 0755
